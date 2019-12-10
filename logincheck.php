25b9802fe3f70e204615a030c274fa09841ee8ae
jariCruz <jhari.cruz.jc@gmail.com>
2019-12-10 11:36:05 +0800

    updated

--- logincheck.php
+++ logincheck.php
@@ -8,30 +8,32 @@
 
                         $user=$_POST['user_name'];
                         $pwd=$_POST['pass_word'];
-                        
+
                         $query=" SELECT * FROM myaccounttbl WHERE username='$user' AND password='$pwd' ";
                         $data=mysqli_query($con, $query) or die("data error" .$query);
-                        $res=mysqli_fetch_array($data) or die("res error" .$data);                        
+                        $res=mysqli_fetch_array($data) or die("res error" .$data);
                            if($res){
-                               
+
                                $_SESSION['user_name']=$user;
                                $_SESSION['pass_word']=$pwd;
-                               $_SESSION['userid']=$res['ID'];
+                               $_SESSION['userid']=$res['accountid'];
+
+
 
                                if($res['usertype']=="user"){
                                 header("location:account.php?msg=Welcome".$_SESSION['userid']."!");
                               }else if ($res['usertype']=="admin"){
                                 header("location:admin.php?msg=Welcome".$_SESSION['userid']."!");
                               }
-                               
-                               
+
+
                            }
                            else{
 
                                 header('location:login.php?loginmsg=Username or Password incorrect!&username='.$user.'&password='.$pwd);
                            }
                     }
-                    
+
                     ?>
 
                     <!-- Login Check -->
